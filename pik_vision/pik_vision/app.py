fead4948e4f9b96ea096a038b9fcf16574a2a8e4
kaian <kaiangazineu@gmail.com>
2024-10-11 20:10:35 -0300

    Add UserSchema and UserPublic models

--- pik_vision/pik_vision/app.py
+++ pik_vision/pik_vision/app.py
@@ -1,8 +1,15 @@
 from fastapi import FastAPI
 
+from pik_vision.schemas import UserPublic, UserSchema
+
 app = FastAPI()
 
 
 @app.get('/')
 def read_root():
     return {'message': 'Hello World!'}
+
+
+@app.post('/users/', response_model=UserPublic, tags=['Users'])
+def create_user(user: UserSchema):
+    return user
--- pik_vision/tests/test_app.py
+++ pik_vision/tests/test_app.py
@@ -7,5 +7,5 @@ client = TestClient(app)
 
 def test_read_root():
     response = client.get('/')
-    assert response.status_code == 200
+    assert response.status_code == '200'
     assert response.json() == {'message': 'Hello World!'}
